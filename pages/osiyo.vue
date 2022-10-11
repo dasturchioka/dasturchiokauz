e452fdf95c83ef31244f0554f42404bcee0015e0
dasturchioka <dasturchioka@gmail.com>
2022-10-11 20:18:12 +0500

    C:/Program Files/Git/osiyo page added

---
+++ pages/osiyo.vue
@@ -0,0 +1,56 @@
+<script></script>
+
+<template>
+  <div class="bg-dark pointer-events-auto sm:px-8 px-4 text-white min-h-screen">
+    <h1 class="text-2xl font-bold pb-4 pt-8">Insonlarning fikrlarini o'qish texnikasi</h1>
+    <p class="leading-7 pb-6">
+      Telepatik qobiliyatlar har bir insonning tabiatiga xosdir. Ularni
+      rivojlantirish uchun ba'zilariga bir necha oy kerak bo'lishi mumkin,
+      boshqalari esa ko'p yillik mashg'ulotlar. Bularning barchasi
+      amaliyotlarning muntazamligi, qat'iyatlilik va tabiiy moyilliklarga
+      bog'liq. O'z ustingizda qattiq mehnat qilsangizgina muvaffaqiyatga
+      erishiladi. Ushbu maqolada biz odamlarning fikrlarini o'qishni o'rganish
+      haqida gaplashamiz. Qadim zamonlardan beri odamlar atrofdagi odamlarning
+      fikrlarini o'qishning ajoyib kuchini egallashga intilishgan. O'limga qadar
+      duelda qatnashgan gladiator raqibning fikrlariga kirib borish uchun ko'p
+      narsani beradi. Dushmandan qaysi vaqtda va qaysi tomondan zarba kutish
+      kerakligini bashorat qilishga urinayotgan komandir zobitlarning hujum
+      rejasi haqidagi fikrlarini bilish imkoniyatidan bosh tortmas edi. Rashkchi
+      xotin haqida nima deyishimiz mumkin, u, albatta, ish kunidan keyin erini
+      qanday "narsalar" ushlab turganini bilishni xohlaydi! Fikrlarni o'qishni
+      qanday o'rganishingiz mumkin: texnika Ba'zi telepatiya qobiliyatlarini
+      o'zlashtirgan odamlar juda ko'p maslahat berishlari va hatto rivojlanishi
+      mumkin o'z dasturi o'rganish. Shuning uchun ko'plab usullar mavjud. Biroq,
+      usullarning har birining mohiyati bittaga qisqartiriladi. Telepatiya,
+      ya'ni. masofadan turib fikrlarni o'qish - bu energiya almashinuvi. Agar
+      biz barcha tavsiyalar va ta'lim tizimlarini umumlashtirsak, unda yagona
+      xulosa o'zini ko'rsatadi: siz bir nechta printsiplarni o'rganish orqali
+      ongni qanday o'qishni o'rganishingiz mumkin. Insonning boshida tug'ilgan
+      har qanday fikr energiya oqimiga aylanadi va yerning axborot maydonining
+      bir qismiga aylanadi. Fikr so'z bilan ifodalanganmi yoki yo'qmi, muhim
+      emas. Energiya axborot signallarini qabul qilish uchun o'zini moslashtira
+      oladigan va ularni lug'at shakliga aylantira oladigan odam telepat
+      hisoblanadi. Telepat qabul qiluvchi kabi ishlaydi. Agar bu tezislar umumiy
+      asos sifatida qabul qilinsa, biz ishonch bilan aytishimiz mumkinki, boshqa
+      odamlarning fikrlarini o'qishni faqat o'zingizda prana his qilish orqali
+      o'rganish haqiqatdir, ya'ni. sayyoramizning axborot maydonining
+      energiyasi. Prana yordamida boshqalarning fikrlarini o'qishni qanday
+      o'rganish mumkin? Yoga va turli xil mashg'ulotlar pranani qabul qilish
+      mahoratini rivojlantirishga yordam beradi. Pranani qabul qilishni
+      o'rgatishning bir usuli quyidagicha ko'rinadi: To'liq dam oling,
+      o'zingizni barcha ishlaringizdan chalg'itishga harakat qiling va sizni
+      bezovta qiladigan narsalarni unuting. Lotus holatida o'tiring. Aynan shu
+      duruş yordamida ichki energiyaning kontsentratsiyasiga hissa qo'shish
+      mumkin. Atrofingizda energiya qanday suzayotganini tasavvur qilishga
+      harakat qiling, keyin energiyani ichkariga kiriting, uni so'rib oling va u
+      bilan birlashing. Ba'zi odamlar bu energiyani issiqlikning kirib borishi,
+      boshqalari esa quyoshning yorqin nurlari deb o'ylashadi.
+    </p>
+    <p class="mb-10">Muallif: <b>Osiyo Abdullayeva</b></p>
+    <nuxt-link to="/" class="bg-green rounded px-4 py-2">Orqaga</nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
